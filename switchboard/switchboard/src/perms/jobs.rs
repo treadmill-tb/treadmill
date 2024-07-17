8bbdfe4acf29c6d9a4c151a78ebf312cbe7399b6
Maximilien Cura <70043540+max-cura@users.noreply.github.com>
2024-07-17 16:37:05 -0700

    Add support for POSTing jobs to /api/v1/job/queue, plus testing fixtures.

--- switchboard/switchboard/src/perms/jobs.rs
+++ switchboard/switchboard/src/perms/jobs.rs
@@ -1,14 +1,19 @@
 //! Privileged actions acting on jobs and the job queue.
 
-use crate::model;
+use crate::model::{Job, JobParameter};
+use crate::perms::{BifurcateProxy, IntoProxiedResponse, ResponseProxy};
 use crate::server::auth::{
-    AuthorizationError, PermissionQueryExecutor, Privilege, PrivilegedAction,
+    AuthSource, AuthorizationError, AuthorizationSource, DbPermSource, PermissionQueryExecutor,
+    Privilege, PrivilegedAction,
 };
 use crate::server::AppState;
-use crate::supervisor::HerdError;
-use axum::async_trait;
+use axum::extract::State;
+use axum::response::{IntoResponse, Response};
+use axum::{async_trait, Json};
+use http::StatusCode;
 use std::fmt::Debug;
-use treadmill_rs::connector::StartJobMessage;
+use treadmill_rs::api::switchboard::{EnqueueJobRequest, EnqueueJobResponse};
+use treadmill_rs::connector::StartJobRequest;
 use uuid::Uuid;
 
 #[derive(Debug, Clone)]
@@ -32,27 +37,24 @@ impl PrivilegedAction for EnqueueCIJobAction {
 pub enum EnqueueJobError {
     Database,
     SupervisorNotFound,
-    Herd(HerdError),
 }
 pub async fn enqueue_ci_job(
     state: &AppState,
     p: Privilege<'_, EnqueueCIJobAction>,
-    job: StartJobMessage,
-    // DELETEME
-    supervisor_id: Uuid,
+    job: StartJobRequest,
 ) -> Result<(), EnqueueJobError> {
     let mut transaction = state.pool().begin().await.map_err(|e| {
         tracing::error!("Failed to create transaction: {e}");
         EnqueueJobError::Database
     })?;
-    model::job::insert(&job, p.subject(), transaction.as_mut())
+    Job::insert(&job, p.subject(), transaction.as_mut())
         .await
         .map_err(|e| {
             tracing::error!("failed to add job ({}) to transaction: {e}", job.job_id);
             EnqueueJobError::Database
         })?;
     let job_id = job.job_id;
-    model::job::params::insert(job_id, job.parameters.clone(), transaction.as_mut())
+    JobParameter::insert(job_id, job.parameters, transaction.as_mut())
         .await
         .map_err(|e| {
             tracing::error!(
@@ -66,15 +68,77 @@ pub async fn enqueue_ci_job(
         EnqueueJobError::Database
     })?;
 
-    // TODO: add job to ephemeral queue
-    state
-        .herd()
-        .try_start_job(job.clone(), supervisor_id)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to start {job:?} on {supervisor_id}: {e}");
-            EnqueueJobError::Herd(e)
-        })?;
+    let subject = p.subject();
+    let object = &p.action().supervisor_id;
+    println!("[subject:{subject:?}] enqueuing a job on [object:{object:?}]");
 
     Ok(())
+}
+
+// ENDPOINTS
+
+impl IntoProxiedResponse for EnqueueJobResponse {
+    fn into_proxied_response(self) -> Response {
+        match self {
+            EnqueueJobResponse::Ok => StatusCode::OK.into_response(),
+            EnqueueJobResponse::SupervisorNotFound => StatusCode::NOT_FOUND.into_response(),
+            EnqueueJobResponse::Unauthorized => StatusCode::UNAUTHORIZED.into_response(),
+            this @ EnqueueJobResponse::Invalid { .. } => {
+                (StatusCode::BAD_REQUEST, Json(this)).into_response()
+            }
+            EnqueueJobResponse::Internal => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        }
+    }
+}
+impl From<EnqueueJobError> for EnqueueJobResponse {
+    fn from(value: EnqueueJobError) -> Self {
+        match value {
+            EnqueueJobError::Database => EnqueueJobResponse::Internal,
+            EnqueueJobError::SupervisorNotFound => EnqueueJobResponse::SupervisorNotFound,
+        }
+    }
+}
+
+pub async fn enqueue(
+    AuthSource(auth_source): AuthSource<DbPermSource>,
+    State(state): State<AppState>,
+    Json(request): Json<EnqueueJobRequest>,
+) -> BifurcateProxy<EnqueueJobResponse> {
+    // check that the supervisor exists (we don't check if it's online)
+
+    let privilege = auth_source
+        .authorize(EnqueueCIJobAction {
+            supervisor_id: request.supervisor_id,
+        })
+        .await
+        .map_err(|e| match e {
+            AuthorizationError::Database(e) => {
+                tracing::error!("failed to check privilege: {e}");
+                EnqueueJobResponse::Internal
+            }
+            AuthorizationError::Unauthorized(p) => {
+                tracing::warn!("{auth_source:?} lacks permission {p}");
+                EnqueueJobResponse::Unauthorized
+            }
+        })
+        .map_err(ResponseProxy)?;
+
+    enqueue_ci_job(&state, privilege, request.start_job_request)
+        .await
+        .map_err(|e| ResponseProxy(EnqueueJobResponse::from(e)))?;
+
+    super::bifurcated_ok!(EnqueueJobResponse::Ok)
+}
+
+pub async fn get_queue() {
+    unimplemented!()
+}
+pub async fn info() {
+    unimplemented!()
+}
+pub async fn cancel() {
+    unimplemented!()
+}
+pub async fn status() {
+    unimplemented!()
 }
