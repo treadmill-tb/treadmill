977e71a9e57aa19657f83460651c5b581ae17077
Maximilien Cura <70043540+max-cura@users.noreply.github.com>
2024-07-18 11:35:11 -0700

    Refactor switchboard code so that structure is more logical, and clean up some inconsistencies and design issues.

---
+++ switchboard/switchboard/src/api.rs
@@ -0,0 +1,29 @@
+pub mod jobs;
+
+use axum::response::{IntoResponse, Response};
+use std::fmt::{Debug, Formatter};
+
+pub trait IntoProxiedResponse {
+    fn into_proxied_response(self) -> Response;
+}
+#[repr(transparent)]
+pub struct ResponseProxy<R: IntoProxiedResponse>(R);
+impl<T: IntoProxiedResponse> IntoResponse for ResponseProxy<T> {
+    fn into_response(self) -> Response {
+        self.0.into_proxied_response()
+    }
+}
+impl<T: IntoProxiedResponse + Debug> Debug for ResponseProxy<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+pub type Bifurcate<T> = Result<T, T>;
+pub type BifurcateProxy<T> = Bifurcate<ResponseProxy<T>>;
+macro_rules! bifurcated_ok {
+    ($t:expr) => {
+        Ok(ResponseProxy($t))
+    };
+}
+use bifurcated_ok;
--- switchboard/switchboard/src/perms/jobs.rs
+++ switchboard/switchboard/src/perms/jobs.rs
@@ -1,18 +1,12 @@
 //! Privileged actions acting on jobs and the job queue.
 
 use crate::model::{Job, JobParameter};
-use crate::perms::{BifurcateProxy, IntoProxiedResponse, ResponseProxy};
 use crate::server::auth::{
-    AuthSource, AuthorizationError, AuthorizationSource, DbPermSource, PermissionQueryExecutor,
-    Privilege, PrivilegedAction,
+    AuthorizationError, PermissionQueryExecutor, Privilege, PrivilegedAction,
 };
 use crate::server::AppState;
-use axum::extract::State;
-use axum::response::{IntoResponse, Response};
-use axum::{async_trait, Json};
-use http::StatusCode;
+use axum::async_trait;
 use std::fmt::Debug;
-use treadmill_rs::api::switchboard::{EnqueueJobRequest, EnqueueJobResponse};
 use treadmill_rs::connector::StartJobRequest;
 use uuid::Uuid;
 
@@ -68,77 +62,7 @@ pub async fn enqueue_ci_job(
         EnqueueJobError::Database
     })?;
 
-    let subject = p.subject();
-    let object = &p.action().supervisor_id;
-    println!("[subject:{subject:?}] enqueuing a job on [object:{object:?}]");
+    // TODO: add job to ephemeral queue
 
     Ok(())
-}
-
-// ENDPOINTS
-
-impl IntoProxiedResponse for EnqueueJobResponse {
-    fn into_proxied_response(self) -> Response {
-        match self {
-            EnqueueJobResponse::Ok => StatusCode::OK.into_response(),
-            EnqueueJobResponse::SupervisorNotFound => StatusCode::NOT_FOUND.into_response(),
-            EnqueueJobResponse::Unauthorized => StatusCode::UNAUTHORIZED.into_response(),
-            this @ EnqueueJobResponse::Invalid { .. } => {
-                (StatusCode::BAD_REQUEST, Json(this)).into_response()
-            }
-            EnqueueJobResponse::Internal => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-        }
-    }
-}
-impl From<EnqueueJobError> for EnqueueJobResponse {
-    fn from(value: EnqueueJobError) -> Self {
-        match value {
-            EnqueueJobError::Database => EnqueueJobResponse::Internal,
-            EnqueueJobError::SupervisorNotFound => EnqueueJobResponse::SupervisorNotFound,
-        }
-    }
-}
-
-pub async fn enqueue(
-    AuthSource(auth_source): AuthSource<DbPermSource>,
-    State(state): State<AppState>,
-    Json(request): Json<EnqueueJobRequest>,
-) -> BifurcateProxy<EnqueueJobResponse> {
-    // check that the supervisor exists (we don't check if it's online)
-
-    let privilege = auth_source
-        .authorize(EnqueueCIJobAction {
-            supervisor_id: request.supervisor_id,
-        })
-        .await
-        .map_err(|e| match e {
-            AuthorizationError::Database(e) => {
-                tracing::error!("failed to check privilege: {e}");
-                EnqueueJobResponse::Internal
-            }
-            AuthorizationError::Unauthorized(p) => {
-                tracing::warn!("{auth_source:?} lacks permission {p}");
-                EnqueueJobResponse::Unauthorized
-            }
-        })
-        .map_err(ResponseProxy)?;
-
-    enqueue_ci_job(&state, privilege, request.start_job_request)
-        .await
-        .map_err(|e| ResponseProxy(EnqueueJobResponse::from(e)))?;
-
-    super::bifurcated_ok!(EnqueueJobResponse::Ok)
-}
-
-pub async fn get_queue() {
-    unimplemented!()
-}
-pub async fn info() {
-    unimplemented!()
-}
-pub async fn cancel() {
-    unimplemented!()
-}
-pub async fn status() {
-    unimplemented!()
 }
