a4c28bb00b3208b4286b3f330f919ae820f42d46
Maximilien Cura <70043540+max-cura@users.noreply.github.com>
2024-07-29 10:06:49 -0700

    Add support for specifying job timeouts

--- treadmill-rs/src/api/switchboard.rs
+++ treadmill-rs/src/api/switchboard.rs
@@ -1,7 +1,10 @@
-use crate::connector::{JobError, JobState, StartJobMessage};
+use crate::api::supervisor_puppet::ParameterValue;
+use crate::api::switchboard_supervisor::{JobInitSpec, RendezvousServerSpec, RestartPolicy};
+use crate::connector::{JobError, JobState};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_with::{base64::Base64, serde_as};
+use std::collections::HashMap;
 use subtle::{Choice, ConstantTimeEq};
 use uuid::Uuid;
 
@@ -38,18 +41,51 @@ pub struct LoginResponse {
     pub expires_at: DateTime<Utc>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+pub struct JobRequest {
+    /// What kind of job this is.
+    pub init_spec: JobInitSpec,
+
+    /// The set of initial SSH keys to deploy onto the image.
+    ///
+    /// The image's configuration of the Treadmill puppet daemon determines
+    /// how and whether these keys will be loaded.
+    pub ssh_keys: Vec<String>,
+
+    pub restart_policy: RestartPolicy,
+
+    /// A set of SSH rendezvous servers to tunnel inbound SSH connections
+    /// through. Leave empty to avoid using SSH rendezvouz
+    /// servers. Supervisors may not support this, in which case they will
+    /// not report back any SSH endpoints reachable through the rendezvous
+    /// endpoints listed here:
+    pub ssh_rendezvous_servers: Vec<RendezvousServerSpec>,
+
+    /// A hash map of parameters provided to this job execution. These
+    /// parameters are provided to the puppet daemon.
+    pub parameters: HashMap<String, ParameterValue>,
+
+    /// The tag configuration.
+    ///
+    /// FIXME: TO BE SPECIFIED
+    pub tag_config: String,
+
+    #[serde(with = "crate::util::chrono::optional_duration")]
+    pub override_timeout: Option<chrono::Duration>,
+}
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct EnqueueJobRequest {
     /// Supervisor to enqueue job on.
     pub supervisor_id: Uuid,
     /// Job request.
-    pub start_job_request: StartJobMessage,
+    pub job_request: JobRequest,
 }
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum EnqueueJobResponse {
     /// Succeeded. (HTTP 200)
-    Ok,
+    Ok { job_id: Uuid },
     /// Requested supervisor does not exist. (HTTP 404)
     SupervisorNotFound,
     /// Authorization subject does not have sufficient privileges. (HTTP 401)
