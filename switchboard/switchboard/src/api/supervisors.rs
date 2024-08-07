e389a3987161e049ecb5297e50531ae3ff216dd6
Maximilien Cura <70043540+max-cura@users.noreply.github.com>
2024-08-07 11:55:20 -0700

    Add GET /api/v1/supervisor/list

--- switchboard/switchboard/src/api/supervisors.rs
+++ switchboard/switchboard/src/api/supervisors.rs
@@ -1,7 +1,18 @@
-use axum::response::Response;
+use crate::server::AppState;
+use axum::extract::State;
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use uuid::Uuid;
 
-pub async fn list() -> Response {
-    todo!()
+pub async fn list(State(app_state): State<AppState>) -> Response {
+    let supervisor_ids: Vec<Uuid> = app_state
+        .scheduler()
+        .herd()
+        .lock_supervisors()
+        .await
+        .keys()
+        .collect();
+    Json(supervisor_ids).into_response()
 }
 
 pub async fn status() -> Response {
